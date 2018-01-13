bebbab5bea675afb34cff4536357859c927410b2
Jeff Goodall <goodallhj@gmail.com>
2018-01-12 20:02:35 -0700

    eslint working

--- webpack.config.js
+++ webpack.config.js
@@ -5,9 +5,9 @@ module.exports = {
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        use: ["babel-loader", "eslint-loader"]
-      }
-    ]
-  }
+        use: ['babel-loader', 'eslint-loader'],
+      },
+    ],
+  },
   // ...
 };
