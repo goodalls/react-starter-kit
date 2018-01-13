8b39a7b4fb4686a66011a4a47bf4ca9f2f97ae69
Jeff Goodall <goodallhj@gmail.com>
2018-01-13 08:38:44 -0700

    added sass to starter kit

--- webpack.config.js
+++ webpack.config.js
@@ -5,9 +5,22 @@ module.exports = {
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        use: ['babel-loader', 'eslint-loader'],
-      },
-    ],
-  },
+        use: ["babel-loader", "eslint-loader"],
+
+        test: /\.scss$/,
+        use: [
+          {
+            loader: "style-loader" // creates style nodes from JS strings
+          },
+          {
+            loader: "css-loader" // translates CSS into CommonJS
+          },
+          {
+            loader: "sass-loader" // compiles Sass to CSS
+          }
+        ]
+      }
+    ]
+  }
   // ...
 };
